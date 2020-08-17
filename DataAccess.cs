18541dbcdeadf27d1f0eed95eca0f481ef79fc92
Dad  S Wonkulah Jr <dadwonkulahjr96@gmail.com>
2020-08-17 10:52:38 +0000

    Accessing the Database using ADO Net Technologies

---
+++ DataAccess.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+
+namespace ConsoleUIDotNet
+{
+    public class DataAccess
+    {
+        public DataAccess()
+        {
+           
+        }
+        public  static void ConnectToDatabase()
+        {
+            int id = 0;
+            string name = null;
+            string gender = null;
+            string email = null;
+            string photoPath = null;
+            try
+            {
+                string cs = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+                using(SqlConnection con = new SqlConnection(cs))
+                {
+                    con.Open();
+                    SqlCommand cmd = new SqlCommand("Select [ID], [Name], [Gender], [Email], [PhotoPath] From Customers", con);
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        id = int.Parse(reader["ID"].ToString());
+                        name = reader["Name"].ToString();
+                        email = reader["Email"].ToString();
+                        gender = reader["Gender"].ToString();
+                        photoPath = reader["PhotoPath"].ToString();
+
+                        Console.WriteLine("Id: " + id);
+                        Console.WriteLine("Name: " + name);
+                        Console.WriteLine("Email: " + email);
+                        Console.WriteLine("Gender: " + gender);
+                        //Console.WriteLine("PhotoPath: " + photoPath);
+                        Console.WriteLine();
+                      
+                    }
+                  
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Data.SqlClient;
+
+
+namespace ConsoleUIDotNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataAccess.ConnectToDatabase();
+        }
+    }
+}
